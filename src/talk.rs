2593a5eb06834521dba1d6341903e54db50daae1
wybxc@qq.com <wybxc@qq.com>
2023-04-11 19:17:49 +0800

    feat: new talk server

--- src/talk.rs
+++ src/talk.rs
@@ -9,12 +9,17 @@ struct Response {
 }
 
 pub async fn get_reponse(talk_server: &str, message: &str) -> Result<String> {
+    let accept = std::env::var("YIRI_ACCEPT")
+        .unwrap_or("-100".to_string())
+        .parse::<f64>()
+        .unwrap_or(-100.0);
+
     let response = reqwest::get(format!("{talk_server}?msg={message}"))
         .await?
         .text()
         .await?;
     let response: Response = serde_json::from_str(&response)?;
-    Ok(if response.score > -0.5 {
+    Ok(if response.score > accept {
         response.result
     } else if rand::random::<f64>() > 0.5 {
         "?".to_string()
